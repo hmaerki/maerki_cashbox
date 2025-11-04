452a7ce05ad305656bf2f18267631924e9bd16b7
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2025-11-04 16:29:03 +0100

    cashboxNet: Add raptux-sharpmt940lib-721357779fec.zip

---
+++ cashboxNet/libraries/mt940/SharpMT940Lib/TransactionDetails.cs
@@ -0,0 +1,23 @@
+﻿namespace Raptorious.SharpMt940Lib
+{
+  /// <summary>
+  /// Details for transaction field :86: (transaction description)
+  /// </summary>
+  public class TransactionDetails
+  {
+    /// <summary>
+    /// Transaction account - IBAN
+    /// </summary>
+    public string Account { get; set; }
+
+    /// <summary>
+    /// Company name, address
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Operation description
+    /// </summary>
+    public string Description { get; set; }
+  }
+}
